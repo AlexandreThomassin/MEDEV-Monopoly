35751ab5de56ed4777daba3facc03c391fd341d2
MayBMatt <matt.bournat@gmail.com>
2023-11-16 09:39:42 +0100

    Remplissage classe "EmplacementConstructible"

--- Monopoly/src/main/java/edu/centrale/monopoly/EmplacementConstructible.java
+++ Monopoly/src/main/java/edu/centrale/monopoly/EmplacementConstructible.java
@@ -9,5 +9,36 @@ package edu.centrale.monopoly;
  * @author mattb
  */
 public class EmplacementConstructible extends Achetable {
+
+    //Constructions sur la case (0 = rien, 1 = une maison, 4 = un hôtel)
+    
+    private int niveau_constr;
+    
+    private int prix_constr;
+        
+    public EmplacementConstructible(String nom, int loyer, int prix, int niveau_constr, int prix_constr) {
+        super(nom, loyer, prix);
+        this.niveau_constr = niveau_constr;
+        this.prix_constr = prix_constr;
+        
+    }
+
+    public EmplacementConstructible(EmplacementConstructible a) {
+        super(a);
+        this.niveau_constr=a.niveau_constr;
+        this.prix_constr=a.prix_constr;
+        
+    }
+
+    public EmplacementConstructible() {
+        super("Emplacement_sans_nom",1,1);
+        this.prix_constr = 1;
+        this.niveau_constr = 1;
+    }
+    
+    //Methode de contruction sur la case
+    public void construire(){
+        
+    }
     
 }
