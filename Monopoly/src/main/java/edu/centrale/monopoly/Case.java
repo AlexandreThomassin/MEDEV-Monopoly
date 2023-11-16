1f50b167ab47ddfbefe682c3ea4580c828e7f79f
Moufid CHALAK <144018760+mchalak2023@users.noreply.github.com>
2023-11-16 08:59:05 +0100

    1ercommit

---
+++ Monopoly/src/main/java/edu/centrale/monopoly/Case.java
@@ -0,0 +1,10 @@
+package edu.centrale.monopoly;
+
+public abstract class Case {
+    private int id;
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
---
+++ Monopoly/src/main/java/edu/centrale/monopoly/Plateau.java
@@ -0,0 +1,19 @@
+package edu.centrale.monopoly;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public abstract class Plateau {
+    private LinkedList<Joueur> joueurs;
+    private ArrayList<Case> cases;
+
+    public void initPlateau(){
+
+    }
+
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
