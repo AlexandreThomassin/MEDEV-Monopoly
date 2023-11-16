11c516874c28e630b2f2f7d6229c8d348c7c2426
Alexandre Thomassin <alex45thomassin@gmail.com>
2023-11-16 08:27:46 +0100

    Add netbeans Project

---
+++ Monopoly/src/main/java/edu/centrale/monopoly/Monopoly.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.centrale.monopoly;
+
+/**
+ *
+ * @author alex4
+ */
+public class Monopoly {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
