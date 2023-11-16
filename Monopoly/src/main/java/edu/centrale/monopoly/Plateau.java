eb1916a2fb16ec746b79c5d04bdaff4fc0a1e2e4
Diego Felipe Lopez Avila <dlopezav@unal.edu.co>
2023-11-16 09:50:22 +0100

    INITV1

--- Monopoly/src/main/java/edu/centrale/monopoly/Plateau.java
+++ Monopoly/src/main/java/edu/centrale/monopoly/Plateau.java
@@ -8,7 +8,23 @@ public abstract class Plateau {
     private ArrayList<Case> cases;
 
     public void initPlateau(){
-
+        this.cases = new ArrayList<Case>(40);
+        cases.add(new Case(0));
+        cases.add(new EmplacementConstructible(0, 0, "MEDITERRANEAN AVENUE", 0, 60, new Joueur(), 1));
+        cases.add(new Chance("COMMUNITY CHEST",2));
+        cases.add(new EmplacementConstructible(0, 0, "BALTIC AVENUE", 0, 60, new Joueur(), 3));
+        cases.add(new Chance("INCOME TAX",4));
+        cases.add(new Gare("READING RAILROAD", 0, 200, new Joueur(), 5));
+        cases.add(new EmplacementConstructible(0, 0, "ORIENTAL AVENUE", 0, 100, new Joueur(), 6));
+        cases.add(new Chance("CHANCE",7));
+        cases.add(new EmplacementConstructible(0, 0, "VERMONT AVENUE", 0, 100, new Joueur(), 8));
+        cases.add(new EmplacementConstructible(0, 0, "CONNECTICUT AVENUE", 0, 120, new Joueur(), 9));
+        cases.add(new Prison(10));
+        cases.add(new EmplacementConstructible(0, 0, "ST. CHARLES PLACE", 0, 140, new Joueur(), 11));
+        cases.add(new EmplacementConstructible(0, 0, "ELECTRIC COMPANY", 0, 150, new Joueur(), 12));
+        cases.add(new EmplacementConstructible(0, 0, "STATES AVENUE", 0, 140, new Joueur(), 13));
+        cases.add(new EmplacementConstructible(0, 0, "VIRGINIA AVENUE", 0, 140, new Joueur(), 13));
+        cases.add(new Gare("PENSYLVANNIA RAILROAD", 0, 200, new Joueur(), 14));
     }
 
 
