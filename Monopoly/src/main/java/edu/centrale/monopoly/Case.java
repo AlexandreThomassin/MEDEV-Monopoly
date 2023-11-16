b7e0e2e64425b10ca1614f8f8defab1ea4679c18
Reda <rfousshi@gmail.com>
2023-11-16 11:31:09 +0100

    nouveau

--- Monopoly/src/main/java/edu/centrale/monopoly/Case.java
+++ Monopoly/src/main/java/edu/centrale/monopoly/Case.java
@@ -2,9 +2,15 @@ package edu.centrale.monopoly;
 
 public abstract class Case {
     private int id;
+ public Case(int id) {
+    this.id = id;
+}
+
 
     @Override
     public String toString() {
         return super.toString();
     }
+ 
+
 }
